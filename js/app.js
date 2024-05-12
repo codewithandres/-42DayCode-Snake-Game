89067db05037904767ba7a236d3837a2c5bc5b66
Dize dev <axiozweb2022@gmail.com>
2024-05-12 10:02:06 -0500

    se creo la funcionalidad de movimiento de la serpiente

--- js/app.js
+++ js/app.js
@@ -5,20 +5,57 @@ let foodX,
     foodY;
 
 let snakeX = 5,
-    snakeY = 10;
+    snakeY = 10,
+    snakeBody = [];
+
+let velocityX = 0,
+    velocityY = 0;
 
 const changeFoodPosition = () => {
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 };
 
+const direcciones = {
+    ARROWUP: 'ArrowUp',
+    ARROWLEFT: 'ArrowLeft',
+    ARROWDOWN: 'ArrowDown',
+    ARROWRIGHT: 'ArrowRight'
+};
+
 const initGame = () => {
     let htmlMarkup = `<div class='food' style='grid-area: ${foodX} / ${foodY}'></div>`;
     htmlMarkup += `<div class='head' style='grid-area: ${snakeX} / ${snakeY}'></div>`;
 
-    playBroad.innerHTML = htmlMarkup;
+    if (snakeX === foodX && snakeY === foodY) {
+        changeFoodPosition()
+        snakeBody.push([foodX, foodY]);
+    };
 
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    playBroad.innerHTML = htmlMarkup;
+};
+
+const changeDirection = (e) => {
+    const { key } = e;
+
+    if (key === direcciones.ARROWUP) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (key === direcciones.ARROWDOWN) {
+        velocityX = 1;
+        velocityY = 0;
+    } else if (key === direcciones.ARROWLEFT) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (key === direcciones.ARROWRIGHT) {
+        velocityX = 0;
+        velocityY = 1;
+    };
 };
 
 changeFoodPosition();
-initGame();
+setInterval(initGame, 125);
+document.addEventListener('keydown', changeDirection);
